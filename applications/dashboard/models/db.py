d45aba86b02e903094cc346b20ab3bc03830a9ba
wantsomechocolate <wantsomechocolate@gmail.com>
2015-07-10 11:36:47 -0400

    one codebase for heroku and beanstalk

--- applications/dashboard/models/db.py
+++ applications/dashboard/models/db.py
@@ -9,22 +9,42 @@
 ## be redirected to HTTPS, uncomment the line below:
 # request.requires_https()
 
+
+
+import os
+
 ## app configuration made easy. Look inside private/appconfig.ini
 from gluon.contrib.appconfig import AppConfig
 ## once in production, remove reload=True to gain full speed
 myconf = AppConfig(reload=True)
 
 
+
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
 
-    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
+    #db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
 
-    # import os
-    # uri = "mysql://%(RDS_USERNAME)s:%(RDS_PASSWORD)s@%(RDS_HOSTNAME)s:%(RDS_PORT)s/%(RDS_DB_NAME)s" % os.environ
-    # db = DAL(uri, pool_size=10)
-    # session.connect(request, response, db=db) # sessions in DB!
 
+    if 'RUNNING_ON_HEROKU' in os.environ:
+        uri=os.environ['WANTSOMEDASHBOARDS_DATABASE_URL']
+        db = DAL(uri, pool_size=10)
+
+    elif 'RUNNING_ON_BEANSTALK' in os.environ:
+        uri = "mysql://%(RDS_USERNAME)s:%(RDS_PASSWORD)s@%(RDS_HOSTNAME)s:%(RDS_PORT)s/%(RDS_DB_NAME)s" % os.environ
+        db = DAL(uri, pool_size=10)
+    
+    ## do whatever you want!
+    else:
+        # db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
+
+        uri=os.environ['WANTSOMEDASHBOARDS_DATABASE_URL']
+        db = DAL(uri, pool_size=10)
+
+        # uri = "mysql://%(RDS_USERNAME)s:%(RDS_PASSWORD)s@%(RDS_HOSTNAME)s:%(RDS_PORT)s/%(RDS_DB_NAME)s" % os.environ
+        # db = DAL(uri, pool_size=10)
+
+    session.connect(request, response, db=db) # sessions in DB!
 
 else:
     ## connect to Google BigTable (optional 'google:datastore://namespace')
