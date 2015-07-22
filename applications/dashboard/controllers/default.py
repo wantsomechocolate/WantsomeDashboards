9aee121a612d73fcae2c1531160f6536c60f6038
wantsomechocolate <wantsomechocolate@gmail.com>
2015-07-22 02:03:03 -0400

    getting started

--- applications/dashboard/controllers/default.py
+++ applications/dashboard/controllers/default.py
@@ -8,6 +8,7 @@
 ## - download is for downloading files uploaded in the db (does streaming)
 #########################################################################
 
+@auth.requires_login()
 def index():
     """
     example action using the internationalization operator T and flash
@@ -16,7 +17,7 @@ def index():
     if you need a simple wiki simply replace the two lines below with:
     return auth.wiki()
     """
-    response.flash = T("Hello World")
+    # response.flash = T("Hello World")
     return dict(message=T('Welcome to web2py!'))
 
 
--- applications/dashboard/static/js/wsd.js
+++ applications/dashboard/static/js/wsd.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    // $('#aws-data-table').DataTable();
-    // $('#example').DataTable();
 
     var url_list=window.location.pathname.split('/')
     var device_id=url_list[url_list.length-1]
@@ -16,27 +14,12 @@ $(document).ready(function() {
 	} );
 
 
-	// var chart = c3.generate({
-	//     bindto: '#chart',
-	//     data: {
-	//       columns: [
-	//         ['data1', 30, 200, 100, 400, 150, 250],
-	//         ['data2', 50, 20, 10, 40, 15, 25]
-	//       ]
-	//     }
-	// });
 
 	// alert(device_id);
 	graph_data = $.ajax({
 		// method:'POST',
 		url: '/ajax_graph_aws_timeseries/'+device_id,
 	}).done(function(response){
-
-		// alert(typeof(response));
-
-		// alert(response.slice(1,response.length-1));
-
-		// alert(JSON.parse(response));
 
 		var chart = c3.generate({
 			bindto:'#chart',
@@ -58,8 +41,6 @@ $(document).ready(function() {
 					    multiline: false
 					},
 					height: 130
-
-
 
 		        }
 		    }
@@ -111,38 +92,6 @@ $(document).ready(function() {
 	//         }
 	//     }
 	// });
-
-
-	// var chart = c3.generate({
-	// 	bindto: '#chart',
-	//     data: {
-	//         x: 'x',
-	// //        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
-	//         columns: [
-	//             ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
-	// //            ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
-	//             ['data1', 30, 200, 100, 400, 150, 250],
-	//             // ['data2', 130, 340, 200, 500, 250, 350]
-	//         ]
-	//     },
-	//     axis: {
-	//         x: {
-	//             type: 'timeseries',
-	//             tick: {
-	//                 format: '%Y-%m-%d'
-	//             }
-	//         }
-	//     }
-	// });
-
-	// setTimeout(function () {
-	//     chart.load({
-	//         columns: [
-	//             ['data3', 400, 500, 450, 700, 600, 500]
-	//         ]
-	//     });
-	// }, 1000);
-
 
 
 } );
